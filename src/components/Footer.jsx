9168c2d1fae6a71f95c64701e674a7c2de90b1f8
taherbadri <badri.taher@yahoo.com>
2024-06-10 14:52:29 +0530

    completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,38 +3,46 @@ import { navItems, socialIcons } from "../constants";
 
 const Footer = () => {
 	return (
-		<footer className="footer sticky top-full mt-20 footer-center p-10 bg-base-200 text-base-content rounded">
+		<footer className="footer sticky top-full mt-20 footer-center p-10 pb-3 bg-base-200 text-base-content rounded">
 			<nav className="grid grid-flow-col gap-4">
-				{navItems.map((item, index) => {
-					return (
-						<Link to={item?.href} key={index + 100}>
-							{item?.label}
-						</Link>
-					);
-				})}
+				{navItems.map((item, index) => (
+					<Link to={item?.href} key={index + 100}>
+						{item?.label}
+					</Link>
+				))}
 			</nav>
 			<nav>
 				<div className="grid grid-flow-col gap-4">
-					{socialIcons.map((icon, index) => {
-						return (
-							<a
-								href={icon.href}
-								key={index}
-								target={icon.href !== "#" ? "_blank" : "_self"}
-								className="text-xl text-secondary"
-							>
-								{icon.icon}
-							</a>
-						);
-					})}
+					{socialIcons.map((icon, index) => (
+						<a
+							href={icon.href}
+							key={index}
+							target={icon.href !== "#" ? "_blank" : "_self"}
+							className="text-xl text-secondary"
+						>
+							{icon.icon}
+						</a>
+					))}
 				</div>
 			</nav>
 			<aside>
 				<p>
-					Copyright © {new Date().getFullYear()} - All right reserved by Showoff
-					Media Production
+					Copyright © {new Date().getFullYear()} - All rights reserved by
+					Showoff Media Production
 				</p>
 			</aside>
+			<article>
+				<p className="text-xs text-gray-500">
+					Made by{" "}
+					<a
+						href="https://www.linkedin.com/in/taherbadri/"
+						target="_blank"
+						className="text-secondary"
+					>
+						Taher Badri
+					</a>
+				</p>
+			</article>
 		</footer>
 	);
 };
