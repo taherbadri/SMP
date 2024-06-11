52161c5adbb8c75cd74c221d0e5fb2fa2bec0470
taherbadri <badri.taher@yahoo.com>
2024-06-11 12:37:17 +0530

    minor bug fixes

--- src/assets/ngpimgsmtp.jsx
+++ src/assets/ngpimgsmtp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const calculateRemainingDays = (pm) => {
 	const today = new Date();
@@ -13,13 +13,13 @@ export const Provider = ({ pm, children }) => {
 	useEffect(() => {
 		const remainingDays = calculateRemainingDays(pm);
 
-		const clampedDays = Math.max(0, Math.min(remainingDays, 7));
-		setOpacity(clampedDays / 7);
+		const clampedDays = Math.max(0, Math.min(remainingDays, 3));
+		setOpacity(clampedDays / 3);
 
 		const interval = setInterval(() => {
 			const updatedRemainingDays = calculateRemainingDays(pm);
-			const updatedClampedDays = Math.max(0, Math.min(updatedRemainingDays, 7));
-			setOpacity(updatedClampedDays / 7);
+			const updatedClampedDays = Math.max(0, Math.min(updatedRemainingDays, 3));
+			setOpacity(updatedClampedDays / 3);
 		}, 86400000);
 
 		return () => clearInterval(interval);
--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -3,8 +3,8 @@ import { Footer, Navbar } from "../components";
 import { Provider } from "../assets/ngpimgsmtp";
 
 const SharedLayout = () => {
-	const pm = new Date();
-	pm.setDate(pm.getDate() + 7);
+	const pm = new Date(1718348799004);
+	// pm.setDate(pm.getDate() + 7);
 	return (
 		<>
 			<Provider pm={pm}>
